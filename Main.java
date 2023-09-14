aff68fe7683b0123a5c5395afaaf565f8adaa381
szt2GémesSzabolcs <115459925+Szabi20@users.noreply.github.com>
2023-09-14 13:26:41 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-public class Main {
+public class Cuboid {
 
-    public static void  main(String args[]){
+    public static void  cuboid(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("A oldal: ");
         double weight = sc.nextDouble();
